15e7de2124c409b5195663496bf37f6a78d154a6
mjbock17 <mjbock@live.com>
2016-11-13 15:38:54 -0600

    Update inputBar.py

--- inputBar.py
+++ inputBar.py
@@ -1,7 +1,7 @@
-    def inputBar(self):    
+    def inputBar(self):    ###Need to create a textbox before using a 
         index = "" ###Blank Message
         while True:
-            p = self.win.getKey() ###Gets key pressed
+            p = self.win.getKey() ###Gets key pressed. Replace win with object
             if p == "Return":
                 return index
             if p == "BackSpace":
@@ -9,4 +9,4 @@
                 self.text.setText(message + " " + index)
                 continue
             index = index + str(p) ###Adds String
-            self.text.setText(message + " " + index)
+            self.text.setText(message + " " + index) ###Replace self.text with made textbox
