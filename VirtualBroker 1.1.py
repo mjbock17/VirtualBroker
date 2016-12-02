640acddd0bae43e53742fc1c59ebcc2b788147e6
alexsocheathson <alexson28@gmail.com>
2016-12-02 14:33:21 -0600

    VirtualBroker V.whateverthefuckwe'reonnow

--- VirtualBroker 1.1.py
+++ VirtualBroker 1.1.py
@@ -1079,6 +1079,7 @@ class lrst(VirtualBroker):
             while True:
                 newin.run()
                 
+###################################################################################################
 class Data_Analysis(VirtualBroker):
     def __init__(self):
         #creates window for GUI
@@ -1086,33 +1087,44 @@ class Data_Analysis(VirtualBroker):
         win.setCoords(0,0,10,10)
         win.setBackground("slategray")
         self.win = win
+        self.__bgimg()
         self.__createButtons()
         self.__createDisplay()
+        self.__makeJerry()
+        self.prevScene = self.setScene()
+
+
+    def __bgimg(self):
+        bg = Image(Point(5,5),"wood.gif")
+        bg.draw(self.win) 
 
     def __createButtons(self):
 
         #creates the list of buttons
-        bsort = [(2.5,6,"Button 1"),(7.5,6,"Button 2"),
-                 (2.5,4.5,"Button 3"),(7.5,4.5,"Button 4")]
+        bsort = [(1, 1, "Back"),(3,6.25,"S&P 500"),(3,5,"Button 2"),
+                 (3,3.75,"Button 3"),(3,2.5,"Button 4")]
         self.buttons = []
         #creates the odd shaped buttons
         for (cx,cy,label) in bsort:
-             self.buttons.append(Button(self.win,Point(cx,cy),4.75,.75,label))
+            if label == "Back":
+                self.buttons.append(Button(self.win, Point(cx, cy), 1, 1, label))
+            else:
+                self.buttons.append(Button(self.win,Point(cx,cy),4,.75,label))
         #activates all buttons
         for b in self.buttons:
             b.activate()
 
     def __makeJerry(self):
-        c = Rectangle(Point(4,2.5),Point(6,7.5))
+        c = Rectangle(Point(6.5,2),Point(8.5,7))
         c.setFill('black')
         c.setOutline('gold')
         c.setWidth(4)
         c.draw(self.win)
-        image = Image(Point(5,5),"myrodin.gif")
+        image = Image(Point(7.5,4.5),"myrodin.gif")
         image.draw(self.win)
-        text2 = Text(Point(5,7.8),"One must spend money to make it")
+        text2 = Text(Point(7.5,1.5),"Please select a Market \n to view its index.")
         text2.setTextColor('white')
-        text2.setSize(10)
+        text2.setSize(15)
         text2.setFace("courier")
         text2.setStyle("bold")
         text2.draw(self.win)
@@ -1121,17 +1133,18 @@ class Data_Analysis(VirtualBroker):
         head = Rectangle(Point(1.5,8),Point(8.5,9))
         head.setFill('white')
         head.draw(self.win)
-        title = Text(Point(5,8.5), "Data Analysis")#
+        title = Text(Point(5,8.5), "Data Analysis")
         title.draw(self.win)
-        title.setSize(15)#
+        title.setSize(15)
         title.setStyle("bold")
-                
+
+    def setScene(self):
+        return VirtualBroker
+
     def processButton(self, key):
-        # Updates the display of the calculator for press of this key
-        text = self.display.getText()
         if key == 'C':
             self.display.setText("")
-        elif key == 'Button 1':
+        elif key == 'S&P 500':
             ans = linSearch(21,mynums)
             self.display.setText(ans)
         elif key == 'Button 2':
@@ -1143,10 +1156,16 @@ class Data_Analysis(VirtualBroker):
         elif key == 'Button 4':
             mergeSort(mynums)
             self.display.setText(mynums)
+        elif key == "Back":
+            self.win.close()
+            newin = self.prevScene()
+            while True:
+                newin.run()
         else:
             # Normal key press, append it to the end of the display
             self.display.setText(text+key)
 
+###################################################################################################
 if __name__ == '__main__':
     theCalc1 = VirtualBroker()
     theCalc1.run()
